6cc8235a7a02edf8fecde929cfcd90a97f64213a
Zlalex <Virdghil300@gmail.com>
2024-12-13 12:37:55 +0300

    Added correct coding-decoding files for solver

--- Config.py
+++ Config.py
@@ -7,12 +7,163 @@ mu_0 = 4 * np.pi * 1e-7  # Магнитная проницаемость вак�
 gamma = 5 / 3  # Адиабатический индекс
 mu = 1.0                 # Магнитная проницаемость
 eta = 0.01               # Магнитная диффузия
-nu = 0.01                # Вязкость
+nu = 0                # Вязкость
 m_D = 3.3437e-27 
 m_T = 5.0075e-27
 m_He = 6.6449e-27
 Lx, Ly = 1.0, 1.0  # Размеры области по x и y
-Nx, Ny = 50, 50    # Число точек сетки
+Nx, Ny = 50, 50   # Число точек сетки
 dx, dy = Lx / Nx, Ly / Ny
 def MevToK(mev):
     return 11604525006.17*mev
+def concatenate_arrays(drho, dV_magnitude, dp, dB, dT, dn_D, dn_T, dn_He):
+    """
+    Concatenate multiple 2D arrays into a single 1D array.
+
+    Parameters:
+    - drho: Change in density (2D array)
+    - dV_magnitude: Change in velocity magnitude (2D array)
+    - dp: Change in pressure (2D array)
+    - dB: Change in magnetic field (2D array)
+    - dT: Change in temperature (2D array)
+    - dn_D: Change in density of D (2D array)
+    - dn_T: Change in density of T (2D array)
+    - dn_He: Change in density of He (2D array)
+
+    Returns:
+    - A single 1D NumPy array containing all concatenated values.
+    """
+    return np.concatenate([
+        drho.flatten(), 
+        dV_magnitude.flatten(), 
+        dp.flatten(), 
+        dB.flatten(), 
+        dT.flatten(), 
+        dn_D.flatten(), 
+        dn_T.flatten(), 
+        dn_He.flatten()
+    ])
+
+def decode_arrays(concatenated_array, Nx, Ny):
+    """
+    Decode a 1D array back into multiple 2D arrays.
+
+    Parameters:
+    - concatenated_array: A single 1D NumPy array containing all values.
+    - Nx: Number of rows.
+    - Ny: Number of columns.
+
+    Returns:
+    - A tuple of eight 2D NumPy arrays corresponding to the original data.
+    """
+    # Calculate the size of each individual array
+    size = Nx * Ny
+    
+    # Extract each array from the concatenated array
+    drho = concatenated_array[0:size].reshape((Nx, Ny))
+    dV_magnitude = concatenated_array[size:size*2].reshape((Nx, Ny))
+    dp = concatenated_array[size*2:size*3].reshape((Nx, Ny))
+    dB = concatenated_array[size*3:size*4].reshape((Nx, Ny))
+    dT = concatenated_array[size*4:size*5].reshape((Nx, Ny))
+    dn_D = concatenated_array[size*5:size*6].reshape((Nx, Ny))
+    dn_T = concatenated_array[size*6:size*7].reshape((Nx, Ny))
+    dn_He = concatenated_array[size*7:size*8].reshape((Nx, Ny))
+
+    return drho, dV_magnitude, dp, dB, dT, dn_D, dn_T, dn_He
+
+def decode_solve_ivp_result(sol, Nx, Ny):
+    """
+    Decode the result from solve_ivp into individual 2D arrays.
+
+    Parameters:
+    - sol: The solution object returned by scipy.integrate.solve_ivp.
+    - Nx: Number of rows (grid points in x-direction).
+    - Ny: Number of columns (grid points in y-direction).
+
+    Returns:
+    - A tuple of eight 2D NumPy arrays corresponding to the original data:
+      (density_change, velocity_magnitude_change, pressure_change, 
+       magnetic_field_change, temperature_change, n_D_change, n_T_change, n_He_change)
+    """
+    # The solution object contains an attribute 'y' which is the concatenated array
+    concatenated_array = sol.y.flatten()  # Flatten to ensure it's a 1D array
+
+    # Calculate the size of each individual array
+    size = Nx * Ny
+    
+    # Extract each array from the concatenated array
+    density_change = concatenated_array[0:size].reshape((Nx, Ny))
+    velocity_magnitude_change = concatenated_array[size:size*2].reshape((Nx, Ny))
+    pressure_change = concatenated_array[size*2:size*3].reshape((Nx, Ny))
+    magnetic_field_change = concatenated_array[size*3:size*4].reshape((Nx, Ny))
+    temperature_change = concatenated_array[size*4:size*5].reshape((Nx, Ny))
+    n_D_change = concatenated_array[size*5:size*6].reshape((Nx, Ny))
+    n_T_change = concatenated_array[size*6:size*7].reshape((Nx, Ny))
+    n_He_change = concatenated_array[size*7:size*8].reshape((Nx, Ny))
+
+    return (density_change, velocity_magnitude_change, pressure_change,
+            magnetic_field_change, temperature_change, n_D_change,
+            n_T_change, n_He_change)
+def extract_frames_from_solution(sol, Nx, Ny):
+    """
+    Extracts frames of data from the solution object returned by solve_ivp.
+
+    Parameters:
+    - sol: The solution object returned by scipy.integrate.solve_ivp.
+    - Nx: Number of rows (grid points in x-direction).
+    - Ny: Number of columns (grid points in y-direction).
+
+    Returns:
+    - A dictionary containing arrays of shape (num_frames, Nx, Ny) for each variable.
+    """
+    # Number of time steps
+    num_frames = sol.y.shape[1]
+
+    # Prepare arrays to hold frames for each variable
+    density_frames = np.zeros((num_frames, Nx, Ny))
+    velocity_magnitude_frames = np.zeros((num_frames, Nx, Ny))
+    pressure_frames = np.zeros((num_frames, Nx, Ny))
+    magnetic_field_frames = np.zeros((num_frames, Nx, Ny))
+    temperature_frames = np.zeros((num_frames, Nx, Ny))
+    n_D_frames = np.zeros((num_frames, Nx, Ny))
+    n_T_frames = np.zeros((num_frames, Nx, Ny))
+    n_He_frames = np.zeros((num_frames, Nx, Ny))
+
+    # Loop through each time step and extract data
+    for i in range(num_frames):
+        # Get the concatenated array for this time step
+        concatenated_array = sol.y[:, i]
+        
+        # Decode the concatenated array into individual variables
+        size = Nx * Ny
+        density_frames[i] = concatenated_array[0:size].reshape((Nx, Ny))
+        velocity_magnitude_frames[i] = concatenated_array[size:size*2].reshape((Nx, Ny))
+        pressure_frames[i] = concatenated_array[size*2:size*3].reshape((Nx, Ny))
+        magnetic_field_frames[i] = concatenated_array[size*3:size*4].reshape((Nx, Ny))
+        temperature_frames[i] = concatenated_array[size*4:size*5].reshape((Nx, Ny))
+        n_D_frames[i] = concatenated_array[size*5:size*6].reshape((Nx, Ny))
+        n_T_frames[i] = concatenated_array[size*6:size*7].reshape((Nx, Ny))
+        n_He_frames[i] = concatenated_array[size*7:size*8].reshape((Nx, Ny))
+
+    return {
+        'density': density_frames,
+        'velocity_magnitude': velocity_magnitude_frames,
+        'pressure': pressure_frames,
+        'magnetic_field': magnetic_field_frames,
+        'temperature': temperature_frames,
+        'n_D': n_D_frames,
+        'n_T': n_T_frames,
+        'n_He': n_He_frames
+    }
+
+# Example usage after solving with solveMe
+# sol = solveMe(duration, initial_data)
+# frames = extract_frames_from_solution(sol, Nx, Ny)
+
+# Access individual frames like this:
+# density_frame_at_t0 = frames['density'][0]
+# velocity_frame_at_t1 = frames['velocity_magnitude'][1]
+
+# Example usage after solving with solveMe
+# sol = solveMe(duration, initial_data)
+# decoded_arrays = decode_solve_ivp_result(sol, Nx, Ny)
--- MGD_Merged.py
+++ MGD_Merged.py
@@ -1,17 +1,22 @@
 from Config import *
 import numpy as np
-from scipy.integrate import solve_ivp
+import scipy.integrate
 from TempDestribution import *
 from tqdm import tqdm
 
-# Граничные условия (замкнутые стенки)
-def apply_boundary_conditions(dencity:np.array, velocity:np.array, magnetic_field:np.array, pressure:np.array):
-    for var in [dencity, velocity, magnetic_field, pressure]:
-        var[0, :] = var[-1, :] = 0
-        var[:, 0] = var[:, -1] = 0
-    return dencity, velocity, magnetic_field, pressure
+# Apply boundary conditions (closed walls)
+def apply_boundary_conditions(density:np.array, velocity:np.array, magnetic_field:np.array, pressure:np.array):
+    density[0, :] = density[-1, :] = 0
+    velocity[0, :] = velocity[-1, :] = 0
+    magnetic_field[0, :] = magnetic_field[-1, :] = 0
+    pressure[0, :] = pressure[-1, :] = 0
+    density[:, 0] = density[:, -1] = 0
+    velocity[:, 0] = velocity[:, -1] = 0
+    magnetic_field[:, 0] = magnetic_field[:, -1] = 0
+    pressure[:, 0] = pressure[:, -1] = 0
+    return density, velocity, magnetic_field, pressure
 
-# Производные по пространству
+# Spatial derivatives
 def grad_x(f):
     return (np.roll(f, -1, axis=0) - np.roll(f, 1, axis=0)) / (2 * dx)
 
@@ -22,40 +27,59 @@ def laplacian(f):
     return (np.roll(f, -1, axis=0) - 2 * f + np.roll(f, 1, axis=0)) / dx**2 + \
            (np.roll(f, -1, axis=1) - 2 * f + np.roll(f, 1, axis=1)) / dy**2
 
-def continuity(dencity:np.array, velocity:np.array) -> np.array:
-    drho_dt = - (grad_x(dencity * velocity) + grad_y(dencity * velocity))
-    return drho_dt
+def continuity(density:np.array, velocity:np.array) ->np.array:
+    return - (grad_x(density * velocity) + grad_y(density * velocity))
 
-# Уравнение движения
-def momentum(dencity:np.array, velocity:np.array, pressure:np.array, magnetic_field:np.array)-> np.array:
-    dV_dt = - velocity * grad_x(velocity) - velocity * grad_y(velocity) \
-            - grad_x(pressure) / dencity + nu * laplacian(velocity)
-    return dV_dt
+# Momentum equation
+def momentum(rho_matrix:np.array, speed_matrix:np.array, p_matrix:np.array, H_matrix:np.array) -> np.array:
+    du_dx = grad_x(speed_matrix)
+    du_dy = grad_y(speed_matrix)
+    
+    dp_dx = grad_x(p_matrix)
+    dp_dy = grad_y(p_matrix)
 
-# Уравнение индукции
-def induction(magnetic_field:np.array, velocity:np.array)-> np.array:
-    dB_dt = grad_x(velocity * magnetic_field) - grad_y(velocity * magnetic_field) + eta * laplacian(magnetic_field)
-    return dB_dt
+    laplace_u = laplacian(speed_matrix)
 
-# Уравнение энергии
-def energy(dencity:np.array, velocity:np.array, pressure:np.array, magnetic_field:np.array)-> np.array:
-    e = pressure / (gamma - 1) + 0.5 * dencity * velocity**2 + 0.5 * magnetic_field**2 / mu
-    de_dt = -grad_x((e + pressure) * velocity) - grad_y((e + pressure) * velocity)
-    return de_dt
+    # Lorentz force components
+    dH_dx = grad_x(H_matrix)
+    dH_dy = grad_y(H_matrix)
+    
+    lorentz_x = H_matrix * dH_dx - grad_x(0.5 * H_matrix**2)
+    lorentz_y = H_matrix * dH_dy - grad_y(0.5 * H_matrix**2)
 
-def F(Popugay,IncData)-> np.array:
-    dencity, velocity, pressure =  np.array(),np.array(),np.array()
-    magnetic_field, Temperature, n_D  = np.array(),np.array(),np.array()
-    n_T , n_He = np.array(), np.array()
-    for var, n  in zip([dencity, velocity, pressure, magnetic_field, Temperature, n_D,  n_T , n_He], range(8)):
-        var = IncData.reshape((8,Nx, Ny))[n]
-    dT, dn_D,  dn_T , dn_He  = UpdateTemp(Temperature, n_D,  n_T , n_He)
-    dE = energy(dencity, velocity, pressure, magnetic_field) + 3 * k_B * Temperature
+    # Time derivatives of velocity components
+    du_dt = (-speed_matrix * (du_dx + du_dy) 
+             - (1 / rho_matrix) * dp_dx 
+             + mu * laplace_u 
+             + lorentz_x / rho_matrix)
+    
+    dv_dt = (-speed_matrix * (du_dx + du_dy) 
+             - (1 / rho_matrix) * dp_dy 
+             + mu * laplacian(speed_matrix) 
+             + lorentz_y / rho_matrix)
+
+    return np.sqrt(du_dt**2 + dv_dt**2)
+
+# Induction equation
+def induction(magnetic_field:np.array, velocity:np.array)->np.array:
+    return grad_x(velocity * magnetic_field) - grad_y(velocity * magnetic_field) + eta * laplacian(magnetic_field)
+
+def F(popugay, IncData:np.array) -> np.array:    
+    density, velocity, pressure, magnetic_field, temperature, n_D, n_T , n_He = decode_arrays(IncData, Nx, Ny)
+
+    dT, dn_D, dn_T , dn_He  = UpdateTemp(temperature, n_D, n_T , n_He)
+    
     dB = induction(magnetic_field, velocity)
-    dV = momentum(dencity, velocity, pressure, magnetic_field)
-    drho = continuity(dencity, velocity)
-    return np.concatenate([drho.ravel(), dV.ravel(), np.zeros((Nx, Ny)).ravel(), dB.ravel(), dT.ravel(), dn_D.ravel(),  dn_T.ravel() , dn_He.ravel()])
+    dp = -grad_x(density * velocity) - grad_y(density * velocity)
+    
+    dV_magnitude = momentum(density, velocity, pressure, magnetic_field)
+    
+    drho = continuity(density, velocity)
 
-def solveMe(Duration, Data):    
-    sol = solve_ivp(F,(0, Duration),Data)
+    # Concatenate results into a single array for integration step
+    x_concatenated = concatenate_arrays(drho, dV_magnitude, dp, dB, dT, dn_D, dn_T, dn_He)
+    return x_concatenated
+
+def solveMe(duration, data:np.array):    
+    sol = scipy.integrate.solve_ivp(F, duration, data)
     return sol
